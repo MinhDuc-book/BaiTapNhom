fc3fda31cf7f5437fcc5faf658c608c28c12b490
Minh Đức <mduc7325@gmail.com>
2025-03-08 10:45:35 +0700

    Update Horse.cpp

--- src/Horse.cpp
+++ src/Horse.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-// direction of Knight
 vector<int> x_pos = {2, 2, -2, -2, 1, 1, -1, -1};
 vector<int> y_pos = {1, -1, 1, -1, 2, -2, 2, -2};
 
-// check for valid
 bool isValid(int x, int y, int n, vector<vector<int>>& board) {
     return (x >= 0 && x < n && y >= 0 && y < n && board[x][y] == -1);
 }
 
 bool findRoad(int x, int y, int count, vector<vector<int>>& board, int n) {
-    if (count == n * n) {
+    if (count == (n * n) + 1) {
         return true;
     }
 
@@ -36,7 +35,7 @@ bool findRoad(int x, int y, int count, vector<vector<int>>& board, int n) {
 void printRoad(const vector<vector<int>>& board, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << board[i][j] << " ";
+            cout << setw(2) << board[i][j] << " ";
         }
         cout << endl;
     }
@@ -44,29 +43,29 @@ void printRoad(const vector<vector<int>>& board, int n) {
 
 int main() {
     int n;
-    cout << "Size of board: ";
+    cout << "Nhap kich thuoc cho ban co: ";
     cin >> n;
     if (n <= 2) {
-        cout << "Size of board too small or not valid";
+        cout << "Kich thuoc ban co qua nho hoac khong hop le";
         return 1;
     }
     vector<vector<int>> board(n, vector<int>(n, -1));
-    
+
     int startX = 0;
     int startY = 0;
-    cout << "Start position of Knight: ";
+    cout << "Nhap toa do ban dau cua con ma (x y): ";
     cin >> startX >> startY;
     if (startX < 0 || startX >= n || startY < 0 || startY >= n) {
-        cout << "Start position not valid";
+        cout << "Vi tri ban dau khong hop le";
         return 1;
     }
-    board[startX][startY] = 0;
+    board[startX][startY] = 1;
 
-    if (findRoad(startX, startY, 1, board, n)) {
-        cout << "Result: " << endl;
+    if (findRoad(startX, startY, 2, board, n)) {
+        cout << "Duong di tim duoc cho con ma là: " << endl;
         printRoad(board, n);
     } else {
-        cout << "Cannot find the road" << endl;
+        cout << "Khong the tim duoc duong di cho ma" << endl;
     }
 
     return 0;
